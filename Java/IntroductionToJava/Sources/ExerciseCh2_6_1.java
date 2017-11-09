7c1ecdcec9fac2befb8ac2791d60806120ebdcb1
Eric <spb828@naver.com>
2017-11-09 18:30:42 +0900

    Update Chapter 2

---
+++ Java/IntroductionToJava/Sources/ExerciseCh2_6_1.java
@@ -0,0 +1,8 @@
+public class ExerciseCh2_6_1 {
+    public static void main(String[] args){
+        System.out.println("\t is  tab.");
+        System.out.println("'\t' is  tab.");
+        System.out.println("\'\t\' is  tab.");
+        System.out.println("'\\t' is  tab.");
+    }
+}
---
+++ Java/IntroductionToJava/Sources/ExerciseCh2_6_2.java
@@ -0,0 +1,6 @@
+public class ExerciseCh2_6_2 {
+    public static void main(String[] args){
+        System.out.println("1/2 = " + 1/2);
+        System.out.println("1/2.0 = " + 1/2.0);
+    }
+}
